eee8826ff47f95166b69b57ad006bd4448ee6918
Javid Asgarov <javid.asgarov@atos.net>
2021-05-09 12:10:07 +0200

    minor refactoring

--- postprocessor/src/main/java/com/asgarov/postprocessor/MyBeanPostProcessor.java
+++ postprocessor/src/main/java/com/asgarov/postprocessor/MyBeanPostProcessor.java
@@ -1,41 +1,38 @@
 package com.asgarov.postprocessor;
 
-import com.asgarov.event.annotation.Loggable;
 import org.slf4j.LoggerFactory;
-import org.springframework.aop.Advisor;
-import org.springframework.aop.TargetSource;
-import org.springframework.aop.framework.ProxyFactory;
-import org.springframework.aop.framework.autoproxy.AutoProxyUtils;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.cglib.proxy.Enhancer;
 import org.springframework.cglib.proxy.MethodInterceptor;
-import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.Optional;
 
 @Component
 public class MyBeanPostProcessor implements BeanPostProcessor {
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-        if (hasLoggableMethods(bean)) {
+        if (hasAnnotatedMethods(bean, PostProcessable.class)) {
             var enhancer = new Enhancer();
             enhancer.setSuperclass(bean.getClass());
-            enhancer.setCallback(getMethodInterception());
+            enhancer.setCallback(createCustomCallback());
             return enhancer.create();
         }
         return bean;
     }
 
-    private boolean hasLoggableMethods(Object bean) {
+    private boolean hasAnnotatedMethods(Object bean, Class<? extends Annotation> clazz) {
         return Arrays.stream(bean.getClass().getMethods())
-                .anyMatch(method -> method.getAnnotation(PostProcessable.class) != null);
+                .map(method -> method.getAnnotation(clazz))
+                .anyMatch(Objects::nonNull);
     }
 
-    public MethodInterceptor getMethodInterception() {
+    public MethodInterceptor createCustomCallback() {
         return (bean, method, objects, methodProxy) -> {
             var annotation = Optional.ofNullable(method.getAnnotation(PostProcessable.class));
             if (annotation.isPresent()) {
